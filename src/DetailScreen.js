3f7ff5730373dae04598ba18c01b78cd836549cb
vidhiigandhi <49667914+vidhiigandhi@users.noreply.github.com>
2020-09-08 18:19:23 +0100

    issues fixed window added in session storage

--- src/DetailScreen.js
+++ src/DetailScreen.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { Jumbotron, Button, Spinner, Figure, Card, Container, Row, Col } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import beer from './assets/beerfill.gif';
-import { Link } from 'react-router-dom';
+import {
+  Jumbotron,
+  Button,
+  Spinner,
+  Figure,
+  Card,
+  Container,
+  Row,
+  Col
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import beer from "./assets/beerfill.gif";
+import { Link } from "react-router-dom";
 
 class DetailScreen extends React.Component {
   constructor(props) {
@@ -13,12 +22,15 @@ class DetailScreen extends React.Component {
       items: []
     };
     this.getRandomBeer = this.getRandomBeer.bind(this);
-    this.id = props.match.params.id.search(':') !== -1 ? props.match.params.id.split(':')[1] : props.match.params.id;
-    sessionStorage.setItem('beerId', this.id);
-    this.url =
-      `http://api.brewerydb.com/v2/beers/?key=a5c1b917e7ba62dcd79f434ed73bc72d&ids=${this.id}&withBreweries=Y`;
+    this.id =
+      props.match.params.id.search(":") !== -1
+        ? props.match.params.id.split(":")[1]
+        : props.match.params.id;
+    window.sessionStorage.setItem("beerId", this.id);
+    this.url = `http://api.brewerydb.com/v2/beers/?key=a5c1b917e7ba62dcd79f434ed73bc72d&ids=${this.id}&withBreweries=Y`;
     this.cors_api_url = "https://cors-anywhere.herokuapp.com/";
-    this.imagefail = 'https://touch.daft.ie/static/images/fallbacks/no-image-size740x480.jpg';
+    this.imagefail =
+      "https://touch.daft.ie/static/images/fallbacks/no-image-size740x480.jpg";
   }
 
   getRandomBeer() {
@@ -48,46 +60,83 @@ class DetailScreen extends React.Component {
     return (
       <Jumbotron>
         <div class="labelBeer"> More about 🍺 </div>
-        {error ?
-          (<div>Error: {error.message}</div>) :
-          (!isLoaded ? (<Spinner animation="border" role="status" variant="warning">
+        {error ? (
+          <div>Error: {error.message}</div>
+        ) : !isLoaded ? (
+          <Spinner animation="border" role="status" variant="warning">
             <span className="sr-only">Loading...</span>
-          </Spinner>) : (
-            <Container>
-              <Row>
-                <Col>
-                  <Link to={'/home'}>Back</Link>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <Card style={{ width: '30rem', backgroundColor: '#ffc107', display: 'inline-block', margin: '10px', padding: '20px' }}>
-                    <Card.Img variant="top" src= {items.labels ? items.labels.medium : this.imagefail} />
-                    <Card.Body>
-                      <Card.Title>{items.name}</Card.Title>
-                      <Card.Text>
-                        <div><div class="titleText">Description:</div> {items.style.description}</div>
-                        <div><div class="titleText">Status:</div> {items.status}</div>
-                        <div><div class="titleText">Category:</div> {items.style.category.name}</div>
-                        <div><div class="titleText">Is Organic:</div> {items.isOrganic == 'N' ? 'No' : 'Yes'}</div>
-                        {items.breweries.map((ele) => {
-                          return (<div>
-                            <div><div class="titleText">Brewery Name:</div> {ele.name}</div>
-                            <div><div class="titleText">Established:</div> {ele.established}</div>
-                            <div><div class="titleText">Website:</div> <a href={`${ele.website}`}>{ele.website}</a></div>
-                            <div><div class="titleText">Brewery Status:</div> {ele.status}</div>
-                          </div>)
-                        })}
-                      </Card.Text>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
-            </Container>
-          ))
-        }
+          </Spinner>
+        ) : (
+          <Container>
+            <Row>
+              <Col>
+                <Link to={"/home"}>Back</Link>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Card
+                  style={{
+                    width: "auto",
+                    backgroundColor: "#ffc107",
+                    display: "inline-block",
+                    margin: "10px",
+                    padding: "20px"
+                  }}
+                >
+                  <Card.Img
+                    variant="top"
+                    src={items.labels ? items.labels.medium : this.imagefail}
+                  />
+                  <Card.Body>
+                    <Card.Title>{items.name}</Card.Title>
+                    <Card.Text>
+                      <div>
+                        <div class="titleText">Description:</div>{" "}
+                        {items.style.description}
+                      </div>
+                      <div>
+                        <div class="titleText">Status:</div> {items.status}
+                      </div>
+                      <div>
+                        <div class="titleText">Category:</div>{" "}
+                        {items.style.category.name}
+                      </div>
+                      <div>
+                        <div class="titleText">Is Organic:</div>{" "}
+                        {items.isOrganic == "N" ? "No" : "Yes"}
+                      </div>
+                      {items.breweries.map((ele) => {
+                        return (
+                          <div>
+                            <div>
+                              <div class="titleText">Brewery Name:</div>{" "}
+                              {ele.name}
+                            </div>
+                            <div>
+                              <div class="titleText">Established:</div>{" "}
+                              {ele.established}
+                            </div>
+                            <div>
+                              <div class="titleText">Website:</div>{" "}
+                              <a href={`${ele.website}`}>{ele.website}</a>
+                            </div>
+                            <div>
+                              <div class="titleText">Brewery Status:</div>{" "}
+                              {ele.status}
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        )}
       </Jumbotron>
-    )
+    );
   }
 }
 
--- src/IntroScreen.js
+++ src/IntroScreen.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { Jumbotron, Button, Spinner, Figure, Card, Container, Row, Col } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import beer from './assets/beerfill.gif';
-import { Link } from 'react-router-dom';
-import history from './history';
+import {
+  Jumbotron,
+  Button,
+  Spinner,
+  Figure,
+  Card,
+  Container,
+  Row,
+  Col
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import beer from "./assets/beerfill.gif";
+import { Link } from "react-router-dom";
+import history from "./history";
 
 class IntroScreen extends React.Component {
   constructor(props) {
@@ -18,7 +27,8 @@ class IntroScreen extends React.Component {
     this.url =
       "http://api.brewerydb.com/v2/beers/?key=a5c1b917e7ba62dcd79f434ed73bc72d&withBreweries=Y&order=random&randomCount=1";
     this.cors_api_url = "https://cors-anywhere.herokuapp.com/";
-    this.imagefail = 'https://touch.daft.ie/static/images/fallbacks/no-image-size740x480.jpg';
+    this.imagefail =
+      "https://touch.daft.ie/static/images/fallbacks/no-image-size740x480.jpg";
   }
 
   getRandomBeer() {
@@ -40,11 +50,10 @@ class IntroScreen extends React.Component {
   }
 
   componentDidMount() {
-    this.id = sessionStorage.getItem('beerId');
-    if(this.id) {
-      sessionStorage.removeItem('beerId');
-      this.url =
-        `http://api.brewerydb.com/v2/beers/?key=a5c1b917e7ba62dcd79f434ed73bc72d&ids=${this.id}&withBreweries=Y`;
+    this.id = window.sessionStorage.getItem("beerId");
+    if (this.id) {
+      window.sessionStorage.removeItem("beerId");
+      this.url = `http://api.brewerydb.com/v2/beers/?key=a5c1b917e7ba62dcd79f434ed73bc72d&ids=${this.id}&withBreweries=Y`;
     }
     this.getRandomBeer();
   }
@@ -54,61 +63,89 @@ class IntroScreen extends React.Component {
     return (
       <Jumbotron>
         <div class="labelBeer"> Welcome To Beer Brewery 🍺 </div>
-        {error ?
-          (<div>Error: {error.message}</div>) :
-          (!isLoaded ? (<Spinner animation="border" role="status" variant="warning">
+        {error ? (
+          <div>Error: {error.message}</div>
+        ) : !isLoaded ? (
+          <Spinner animation="border" role="status" variant="warning">
             <span className="sr-only">Loading...</span>
-          </Spinner>) : (
-            <Container>
-              <Row>
-                <Col>
-                  <Figure>
-                    <Figure.Image
-                      width={210}
-                      height={210}
-                      alt="210x210"
-                      src={beer}
-                    />
-                    <Figure.Caption>
-                      Image Source: <a href="https://blog.joypixels.com/presenting-emoji-animations-2-0/">Joy Pixels</a>
-                    </Figure.Caption>
-                  </Figure>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <Button variant="warning" class="titleText" onClick={() => {
+          </Spinner>
+        ) : (
+          <Container>
+            <Row>
+              <Col>
+                <Figure>
+                  <Figure.Image
+                    width={210}
+                    height={210}
+                    alt="210x210"
+                    src={beer}
+                  />
+                  <Figure.Caption>
+                    Image Source:{" "}
+                    <a href="https://blog.joypixels.com/presenting-emoji-animations-2-0/">
+                      Joy Pixels
+                    </a>
+                  </Figure.Caption>
+                </Figure>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Button
+                  variant="warning"
+                  class="titleText"
+                  onClick={() => {
                     this.url =
                       "http://api.brewerydb.com/v2/beers/?key=a5c1b917e7ba62dcd79f434ed73bc72d&withBreweries=Y&order=random&randomCount=1";
                     this.getRandomBeer();
-                  }
-                  }>Click for Random Beer</Button>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <Card style={{ width: 'auto', backgroundColor: '#ffc107', display: 'inline-block', margin: '10px', padding: '20px' }}>
-                    <Card.Img variant="top" src= {items.labels ? items.labels.medium : this.imagefail} />
-                    <Card.Body>
-                      <Card.Title>{items.name}</Card.Title>
-                      <Card.Text>
-                        <div><div class="titleText">Description:</div> {items.style.description}</div>
-                        {items.breweries.map((ele) => {
-                          return (<div>
-                            <div><div class="titleText">Brewery Name:</div> {ele.name}</div>
-                          </div>)
-                        })}
-                      </Card.Text>
-                      <Link to={`/details/:${items.id}`}>More Details..</Link>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
-            </Container>
-          ))
-        }
+                  }}
+                >
+                  Click for Random Beer
+                </Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Card
+                  style={{
+                    width: "auto",
+                    backgroundColor: "#ffc107",
+                    display: "inline-block",
+                    margin: "10px",
+                    padding: "20px"
+                  }}
+                >
+                  <Card.Img
+                    variant="top"
+                    src={items.labels ? items.labels.medium : this.imagefail}
+                  />
+                  <Card.Body>
+                    <Card.Title>{items.name}</Card.Title>
+                    <Card.Text>
+                      <div>
+                        <div class="titleText">Description:</div>{" "}
+                        {items.style.description}
+                      </div>
+                      {items.breweries.map((ele) => {
+                        return (
+                          <div>
+                            <div>
+                              <div class="titleText">Brewery Name:</div>{" "}
+                              {ele.name}
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </Card.Text>
+                    <Link to={`/details/:${items.id}`}>More Details..</Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        )}
       </Jumbotron>
-    )
+    );
   }
 }
 
